458958f2c91201ee54927fa7c7de5fd203ceca3c
Vlad Rochian <vlad.rochian@gmail.com>
2020-08-11 17:43:48 +0300

    Implement optimized Levenshtein

--- sars_cov_2/find_altered_spike_length.py
+++ sars_cov_2/find_altered_spike_length.py
@@ -1,7 +1,7 @@
 import sys
 
 from sars_cov_2.spike import extract_spike
-from util.algorithms import generate_diff
+from util.algorithms import generate_diff, lev_distance_optimized
 from util.fasta_utils import *
 
 reference_file = sys.argv[1]
@@ -9,18 +9,21 @@ input_file = sys.argv[2]
 output_file = sys.argv[3]
 
 ref_spike = read_single_sequence(reference_file)
+max_mutations = 40
 
 with open(output_file, 'w') as f:
     total_seq = 0
     useful_seq = 0
 
+    dif_max = 0
     for header, seq in read_sequences(input_file):
         total_seq += 1
         spike = extract_spike(seq)
-        if total_seq % 100 == 0:
+        if total_seq % 200 == 0:
             print('{} processed sequences'.format(total_seq))
-        if len(ref_spike) != len(spike) and 20 < len(spike) < 2 * len(ref_spike) and spike.find('N') == -1:
+        dif = abs(len(ref_spike) - len(spike))
+        if 0 < dif < max_mutations:
             useful_seq += 1
-            f.write(header + '\n' + generate_diff(ref_spike, spike))
+            f.write(header + '\n' + str(lev_distance_optimized(ref_spike, spike, max_mutations, ignore_n=True)) + '\n')
 
     print('{} total sequences\n{} useful sequences'.format(total_seq, useful_seq))
--- util/algorithms.py
+++ util/algorithms.py
@@ -11,7 +11,18 @@ def extract_first_match(s: str, starts_with: str, ends_with: str, search_from=1)
     return suffix[:suffix.find(ends_with) + len(ends_with)]
 
 
-def lev_distance(s1: str, s2: str) -> int:
+def char_dist(c1: str, c2: str, ignore_n: bool) -> int:
+    return 0 if c1 == c2 or (ignore_n and (c1 == 'N' or c2 == 'N')) else 1
+
+
+def at(ls: list, index: int, default):
+    try:
+        return ls[index]
+    except IndexError:
+        return default
+
+
+def lev_distance(s1: str, s2: str, ignore_n=False) -> int:
     """
     Levenshtein distance between two sequences.
 
@@ -21,6 +32,7 @@ def lev_distance(s1: str, s2: str) -> int:
 
     :param s1: first sequence
     :param s2: second sequence
+    :param ignore_n: if true, the function will ignore differences given by letter N in the sequences
     :return: edit distance
     """
     best = [[0 for _ in range(len(s2) + 1)] for _ in range(2)]
@@ -30,11 +42,51 @@ def lev_distance(s1: str, s2: str) -> int:
         best[i & 1][0] = i
         for j in range(1, len(s2) + 1):
             best[i & 1][j] = min(best[(i & 1) ^ 1][j] + 1, best[i & 1][j - 1] + 1,
-                                 best[(i & 1) ^ 1][j - 1] + (0 if s1[i - 1] == s2[j - 1] else 1))
+                                 best[(i & 1) ^ 1][j - 1] + char_dist(s1[i - 1], s2[j - 1], ignore_n))
     return best[len(s1) & 1][len(s2)]
 
 
-def generate_diff(s1: str, s2: str) -> str:
+def lev_distance_optimized(s1: str, s2: str, upper_bound: int, ignore_n=False) -> int:
+    """
+    Levenshtein distance between two sequences, optimized.
+
+    Time complexity: |s1|
+
+    Memory complexity: 1
+
+    :param s1: first sequence
+    :param s2: second sequence
+    :param upper_bound: maximum estimated value of the difference
+    :param ignore_n: if true, the function will ignore differences given by letter N in the sequences
+    :return: edit distance; if it is greater than the upper_bound, -1 is returned
+    """
+    inf = 2 * upper_bound
+    best = [[0 for _ in range(2 * upper_bound + 1)] for _ in range(2)]
+    for i in range(upper_bound):
+        best[0][i] = inf
+    for i in range(upper_bound + 1):
+        best[0][i + upper_bound] = i
+    for i in range(1, len(s1) + 1):
+        flag = False
+        start = max(0, upper_bound - i)
+        end = min(2 * upper_bound + 1, upper_bound + len(s2) - i + 1)
+        for j in range(start):
+            best[i & 1][j] = inf
+        for j in range(start, end):
+            best[i & 1][j] = min(best[(i & 1) ^ 1][j + 1] + 1 if j < 2 * upper_bound else inf,
+                                 best[i & 1][j - 1] + 1 if j > 0 else inf,
+                                 best[(i & 1) ^ 1][j] + char_dist(s1[i - 1], s2[j + i - upper_bound - 1], ignore_n))
+            if best[i & 1][j] <= upper_bound:
+                flag = True
+        if not flag:
+            return -1
+        for j in range(end, 2 * upper_bound + 1):
+            best[i & 1][j] = inf
+    ans = best[len(s1) & 1][len(s2) - len(s1) + upper_bound]
+    return ans if ans <= upper_bound else -1
+
+
+def generate_diff(s1: str, s2: str, ignore_n=False) -> str:
     """
     Generate graphically aligned strings with highlighted differences.
 
@@ -44,6 +96,7 @@ def generate_diff(s1: str, s2: str) -> str:
 
     :param s1:
     :param s2:
+    :param ignore_n: if true, the function will ignore differences given by letter N in the sequences
     :return: printable representation of differences
     """
     best = [[0 for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
@@ -55,7 +108,7 @@ def generate_diff(s1: str, s2: str) -> str:
         best[i][0] = i
         prv[i][0] = i - 1, 0
         for j in range(1, len(s2) + 1):
-            best[i][j] = best[i - 1][j - 1] + (0 if s1[i - 1] == s2[j - 1] else 1)
+            best[i][j] = best[i - 1][j - 1] + char_dist(s1[i - 1], s2[j - 1], ignore_n)
             prv[i][j] = i - 1, j - 1
             if best[i - 1][j] + 1 < best[i][j]:
                 best[i][j] = best[i - 1][j] + 1
@@ -71,7 +124,7 @@ def generate_diff(s1: str, s2: str) -> str:
     while i > 0 or j > 0:
         prv_i, prv_j = prv[i][j]
         if prv_i == i - 1 and prv_j == j - 1:
-            first_line.append(' ' if s1[i - 1] == s2[j - 1] else 'v')
+            first_line.append(' ' if char_dist(s1[i - 1], s2[j - 1], ignore_n) == 0 else 'v')
             second_line.append(s1[i - 1])
             third_line.append(s2[j - 1])
         elif prv_i == i - 1:
