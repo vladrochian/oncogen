2afdc22c7472ed3fb45008c644c02cd23a4de458
LittleGreenMen-1 <mihai.albu2003@gmail.com>
2020-08-13 22:48:35 +0300

    Added support for all types of mutations

--- sars_cov_2/mutations.py
+++ sars_cov_2/mutations.py
@@ -1,29 +1,46 @@
-"""
-Nomenclature:
-  Substitution: nucleotide position nucleotide
-    ex: A543T
-  Deletion: position 'del' nucleotide
-    ex: 546delT
-            position '_' position 'del'
-    ex: 344_456del
-  Insertion: position '_' position 'ins' nucleotides
-    ex: 455_457insAT
-"""
+from util.algorithms import generate_diff
+import re
 
-def find_mutations(ref_seq: str, seq: str) -> str:
-  """
-  Returns a list of mutations that happend on seq
 
-  :param ref_seq: Refrence sequence, to which we compare the mutated sequence
-  :param seq: The mutated sequence
-  :return: A list of mutations, in the order that they appear
-  """
+def find_mutations(ref_seq: str, seq: str) -> list:
+    """
+    Returns a list of mutations that happened on seq
 
-  best = [[0 for _ in range(len(ref_seq))] for _ in range(2)]
-  aligned_ref_seq = ""
-  aligned_seq = ""
+    :param ref_seq: Reference sequence, to which we compare the mutated sequence
+    :param seq: The mutated sequence
+    :return: A list of mutations, substitutions first, than insertions and deletions
+             in the order that they appear
+    """
 
-  # If substitution: add nucleotides to aligned_ref_seq and aligned_seq
-  # If deletion: add '-' to aligned_seq
-  # If insertion: add '-' to aligned_ref_seq
+    mutations = []
 
+    changes, ref_seq, seq, junk = generate_diff(ref_seq, seq).split('\n')
+
+    last_pos = -1
+    while (subst_pos := changes[last_pos + 1:].find("v")) != -1:
+        subst_pos += last_pos + 1
+
+        mutations.append(f"{ref_seq[subst_pos]}{subst_pos}{seq[subst_pos]}")
+        last_pos = subst_pos
+
+    last_pos = -1
+    while (subst_pos := changes[last_pos + 1:].find("*")) != -1:
+        subst_pos += last_pos + 1
+        reg_match = re.search(r'[^\*]', changes[subst_pos:])
+
+        if reg_match:
+            subst_end_pos = reg_match.start() + subst_pos
+        else:
+            subst_end_pos = len(changes)
+
+        if re.fullmatch(r' *', ref_seq[subst_pos: subst_end_pos]):
+            mutations.append(f"{subst_pos}_{subst_end_pos}ins{seq[subst_pos: subst_end_pos]}")
+        else:
+            if subst_pos == subst_end_pos - 1:
+                mutations.append(f"{subst_pos}del{ref_seq[subst_pos]}")
+            else:
+                mutations.append(f"{subst_pos}_{subst_end_pos - 1}del")
+
+        last_pos = last_pos + subst_end_pos
+
+    return mutations
