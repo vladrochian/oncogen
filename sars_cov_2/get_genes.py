7807781a0fa8d7d54bc4efc0944f7606d9a598f9
Vlad Rochian <vlad.rochian@gmail.com>
2020-11-14 15:57:02 +0200

    Add patterns for other genes

--- sars_cov_2/get_genes.py
+++ sars_cov_2/get_genes.py
@@ -7,7 +7,17 @@ from util.gene_utils import GenePattern, GeneDetails, get_genes, genes_to_string
 
 def get_sars_cov_2_gene_patterns():
     return {
-        'spike': GenePattern('ATGTTTGT', 'ACACATAA', 21400)
+        'orf1ab': GenePattern('ATGGAGAGC', 'AACAACTAA', 200),
+        'spike': GenePattern('ATGTTTGT', 'ACACATAA', 21400),
+        'orf3a': GenePattern('ATGGATTTG', 'CCTTTGTAA', 25300),
+        'E': GenePattern('ATGTACTCA', 'CTGGTCTAA', 26200),
+        'M': GenePattern('ATGGCAGAT', 'GTACAGTAA', 26500),
+        'orf6': GenePattern('ATGTTTCAT', 'ATTGATTAA', 27150),
+        'orf7a': GenePattern('ATGAAAATT', 'ACAGAATGA', 27300),
+        'orf7b': GenePattern('ATGATTGAA', 'CACGCCTAA', 27700),
+        'orf8': GenePattern('ATGAAATTT', 'TTCATCTAA', 27800),
+        'N': GenePattern('ATGTCTGAT', 'CAGGCCTAA', 28200),
+        'orf10': GenePattern('ATGGGCTAT', 'CTCACATAG', 29500)
     }
 
 
@@ -35,7 +45,12 @@ def extract_mutations_per_gene_per_sequence(
             mutations = get_substitution_mutations_per_gene(ref_genes, genes)
             f.write(header + '\n')
             for gene, mutation_list in mutations.items():
-                f.write(gene + ': ' + ', '.join(mutation_list) + '\n')
+                len_diff = len(genes[gene]) - len(ref_genes[gene])
+                f.write('{}{}: {}\n'.format(
+                    gene,
+                    ' [{}]'.format(len_diff) if len_diff != 0 else '',
+                    ', '.join(mutation_list)
+                ))
             f.write('\n')
 
 
